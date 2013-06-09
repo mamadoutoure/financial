138de90fd59b6a42b23ab817017adb97670d5d1f
Thai Le <lnthai2002@gmail.com>
2013-06-09 01:10:41 -0400

    issue_61: use RSpec and FactoryGirl when generate model and controller

--- lib/financial/engine.rb
+++ lib/financial/engine.rb
@@ -1,5 +1,11 @@
 module Financial
   class Engine < ::Rails::Engine
     isolate_namespace Financial
+    config.generators do |g|
+      g.test_framework      :rspec,        :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
 end
