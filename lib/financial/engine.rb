34ec5d408d958d2d16d993f75eee1646ed6a5db3
Thai Le <lnthai2002@gmail.com>
2013-06-09 01:42:57 -0400

    issue_3: add tab_on_rails as dependency of this engine

--- lib/financial/engine.rb
+++ lib/financial/engine.rb
@@ -1,6 +1,8 @@
 module Financial
   class Engine < ::Rails::Engine
     isolate_namespace Financial
+    require 'rubygems'
+    require 'tabs_on_rails'
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
