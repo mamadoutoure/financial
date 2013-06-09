45183f5283c6759a1bb71f10c1bed86fa8454e0c
Thai Le <lnthai2002@gmail.com>
2013-06-09 01:22:18 -0400

    issue_3: copy routes from PAS

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Financial::Engine.routes.draw do
+  resources :payment_types
+
+  match 'expenses/:year/:month' => 'expenses#index', :defaults=>{:year=>nil,:month=>nil}, :conditions=>{:method=>:get}
+  resources :exp_types
+
+  resources :expenses
 end
