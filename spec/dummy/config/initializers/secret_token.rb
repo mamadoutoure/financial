38a68f89f4211d57b859c3fa7c216c92eb3db7b6
Thai Le <lnthai2002@gmail.com>
2013-06-09 00:41:30 -0400

    initial commit, just create rails engine for financial

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c56c8b42cbb6cd5196595a89b3eb3a8e31d789ae66cf05e94691dfebd5675389c120fca6e1e198f858c8cdcca935c52814bdc4fa2520ed2dbfadbd56f5c9363b'
