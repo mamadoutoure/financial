78770f4a3cf801becc2ec309a4081c5057e9684e
Thai Le <lnthai2002@gmail.com>
2013-06-12 23:33:11 -0400

    issue_55: invesment and mortgage

---
+++ app/models/financial/invesment.rb
@@ -0,0 +1,15 @@
+module Financial
+  class Invesment < ActiveRecord::Base
+    attr_accessible :principal, :rate, :monthly_dep, :months 
+
+    def future_value
+      percentage_rate = rate / 100
+      yearly_dep = monthly_dep * 12
+      
+      future_value_of_principal = principal*(1+percentage_rate)**(months/12)
+      future_value_of_monthly_dep = yearly_dep*(((1+percentage_rate)**(months/12)) -1)/percentage_rate
+      
+      return (future_value_of_monthly_dep + future_value_of_principal)
+    end
+  end
+end
