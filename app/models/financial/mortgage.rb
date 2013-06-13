35c36a72b9ca3603d515a976aa0708548be0c5db
Thai Le <lnthai2002@gmail.com>
2013-06-12 20:03:20 -0400

    issue_55: cal interest fater x year

--- app/models/financial/mortgage.rb
+++ app/models/financial/mortgage.rb
@@ -3,7 +3,11 @@ module Financial
     attr_accessible :init_val, :int_rate, :monthly_dep, :inv_months
 
     def future_value
-      return init_val*((1 + int_rate)**inv_months - 1)/int_rate
+      yearly_dep = monthly_dep * 12
+      #i = int_rate/12 #monthly interest rate
+      future_value_of_init_val = init_val*(1+int_rate)**(inv_months/12)
+      future_value_of_monthly_dep = yearly_dep*(((1+int_rate)**(inv_months/12)) -1)/int_rate
+      return (future_value_of_monthly_dep + future_value_of_init_val)
     end
   end
 end
