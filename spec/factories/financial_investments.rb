1c12090c04776d02e44c43094be76842afa948ce
Thai Le <lnthai2002@gmail.com>
2013-06-14 11:36:05 -0400

    issue_55: fix typo in investment

--- spec/factories/financial_investments.rb
+++ spec/factories/financial_investments.rb
@@ -1,6 +1,6 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :financial_invesment, :class => 'Invesment' do
+  factory :financial_investment, :class => 'Investment' do
   end
 end
