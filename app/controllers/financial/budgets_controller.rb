6c558acc9b64513a03dc5ecce2045c8800c7b05a
Thai Le <lnthai2002@gmail.com>
2013-06-15 00:20:34 -0400

    issue_55: add budget listing and controller

--- app/controllers/financial/budgets_controller.rb
+++ app/controllers/financial/budgets_controller.rb
@@ -2,5 +2,17 @@ require_dependency "financial/application_controller"
 
 module Financial
   class BudgetsController < ApplicationController
+    def index
+      @budget = Budget.new
+      @budget.build_mortgage
+      @budget.build_investment
+      @budgets = Budget.all
+    end
+
+    def create
+      @budget = params[:budget]
+      @budget.save
+      render "add_new_budget"
+    end
   end
 end
