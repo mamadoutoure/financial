cab9ad21f0bdf747e9e30f5b595934fb50fd03fb
Thai Le <lnthai2002@gmail.com>
2013-06-16 10:02:15 -0400

    issue_55: destroy dependencies

--- app/models/financial/budget.rb
+++ app/models/financial/budget.rb
@@ -1,8 +1,8 @@
 module Financial
   class Budget < ActiveRecord::Base
     attr_accessible :name, :total_asset
-    has_one :mortgage
-    has_one :investment
+    has_one :mortgage, :dependent => :destroy
+    has_one :investment, :dependent => :destroy
     #it can have more, TODO: make it polymophic association
     accepts_nested_attributes_for :mortgage
     accepts_nested_attributes_for :investment
