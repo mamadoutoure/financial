5c922cb8f9579faa2a6e59d1a806129aa5ab0674
Thai Le <lnthai2002@gmail.com>
2013-06-16 10:05:20 -0400

    issue_55: set budget name automatically if blank

--- app/models/financial/budget.rb
+++ app/models/financial/budget.rb
@@ -1,10 +1,18 @@
 module Financial
   class Budget < ActiveRecord::Base
     attr_accessible :name, :total_asset, :mortgage_attributes, :investment_attributes
+    before_save :set_values
     has_one :mortgage, :dependent => :destroy
     has_one :investment, :dependent => :destroy
     #it can have more, TODO: make it polymophic association
     accepts_nested_attributes_for :mortgage
     accepts_nested_attributes_for :investment
+
+    protected
+    def set_values
+      if self.name.blank?
+        self.name = Time.now.to_s
+      end
+    end
   end
 end
