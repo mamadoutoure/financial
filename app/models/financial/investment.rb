ef1181ff6033123dd273b734d780a258e70d6b24
Thai Le <lnthai2002@gmail.com>
2013-06-16 10:10:48 -0400

    issue_55: virtual attributes for investment to calculate alternative future value

--- app/models/financial/investment.rb
+++ app/models/financial/investment.rb
@@ -1,7 +1,26 @@
 module Financial
   class Investment < ActiveRecord::Base
-    attr_accessible :principal, :rate, :monthly_dep, :months 
+    attr_accessible :principal, :rate, :monthly_dep, :months, :alt_rate, :alt_monthly_dep, :alt_length
+    #virtual attributes
+    attr_reader :alt_rate, :alt_monthly_dep, :alt_length
+    
     belongs_to :budget
+    attr_reader :foo
+
+    #setter for virtual attributes with auto cast
+    #see http://stackoverflow.com/questions/11561141/type-cast-an-activerecord-model-virtual-attribute
+    def alt_rate=(value)
+      @alt_rate = ActiveRecord::ConnectionAdapters::Column.value_to_decimal(value)
+    end
+
+    def alt_monthly_dep=(value)
+      @alt_monthly_dep = ActiveRecord::ConnectionAdapters::Column.value_to_decimal(value)
+    end
+
+    def alt_length=(value)
+      @alt_length = ActiveRecord::ConnectionAdapters::Column.value_to_decimal(value)
+    end
+
     def future_value
       percentage_rate = rate / 100
       yearly_dep = monthly_dep * 12
