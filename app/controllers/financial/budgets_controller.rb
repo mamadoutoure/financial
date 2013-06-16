edf596679dd679c413fef39980b5ac7d3a494370
Thai Le <lnthai2002@gmail.com>
2013-06-16 09:58:03 -0400

    issue_55: fix create budget form not working due to object not instantiated

--- app/controllers/financial/budgets_controller.rb
+++ app/controllers/financial/budgets_controller.rb
@@ -4,13 +4,14 @@ module Financial
   class BudgetsController < ApplicationController
     def index
       @budget = Budget.new
+      #see http://stackoverflow.com/questions/4867880/nested-attributes-in-rails-3
       @budget.build_mortgage
       @budget.build_investment
       @budgets = Budget.all
     end
 
     def create
-      @budget = params[:budget]
+      @budget = Budget.new(params[:budget])
       @budget.save
       render "add_new_budget"
     end
