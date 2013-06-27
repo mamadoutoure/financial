8646ee2808a8c15708d33234ac760cafc9e07140
Thai Le <lnthai2002@gmail.com>
2013-06-27 15:08:31 -0400

    issue_58: declare financial money type for columns in budget, investement, mortgage, mortgage adjustment

--- db/migrate/20130614021611_create_financial_budgets.rb
+++ db/migrate/20130614021611_create_financial_budgets.rb
@@ -2,7 +2,7 @@ class CreateFinancialBudgets < ActiveRecord::Migration
   def change
     create_table :financial_budgets do |t|
       t.column :name, :string
-      t.column :total_asset, :decimal, :precision=>12, :scale=>2
+      t.money :total_asset
       t.timestamps
     end
   end
