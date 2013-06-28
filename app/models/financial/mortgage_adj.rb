c0031d99ba1348e0150ff50c433b5bd5cd4905ec
Thai Le <lnthai2002@gmail.com>
2013-06-27 23:12:14 -0400

    issue_57: interest rate change must happen on the first month of the year

--- app/models/financial/mortgage_adj.rb
+++ app/models/financial/mortgage_adj.rb
@@ -10,9 +10,16 @@ module Financial
     validates :month, :presence => true
     validates :amount, :interest, :numericality => true , :allow_blank=>true
     validates :month, :numericality => { :only_integer => true } , :allow_blank=>true
+    validate :rate_adj_must_be_on_first_month_of_year
 
     protected
     
+    def rate_adj_must_be_on_first_month_of_year
+      if !interest.blank? && month%12 != 1
+        errors.add(:month, "should be the first month of the year the interest rate change")
+      end 
+    end
+
     def rate
       @r = self.interest/100 if @r.blank?
       return @r
