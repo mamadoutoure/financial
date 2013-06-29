5dd6090222e998f262f795aacbe120871240b93a
Thai Le <lnthai2002@gmail.com>
2013-06-29 14:19:24 -0400

    issue_62: check user finance on every request

--- app/controllers/financial/application_controller.rb
+++ app/controllers/financial/application_controller.rb
@@ -1,7 +1,18 @@
 module Financial
   class ApplicationController < ActionController::Base
     before_filter RubyCAS::Filter
+    before_filter :check_user_finance
 
     layout 'layouts/application'
+
+    protected
+
+    #Make sure all user entering finance module must have Finance
+    def check_user_finance
+      @person = Person.where(:email=>session[:cas_user]).first
+      if @person.finance.blank?
+        redirect_to new_finance_path
+      end
+    end
   end
 end
