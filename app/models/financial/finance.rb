429d52299cf4c01dae9eb2dd2d900d9e5f9f5e9e
Thai Le <lnthai2002@gmail.com>
2013-06-29 19:31:51 -0400

    issue_62: fix typo with net_monthly_income column in Finance

--- app/models/financial/finance.rb
+++ app/models/financial/finance.rb
@@ -1,9 +1,9 @@
 module Financial
   class Finance < ActiveRecord::Base
-    monetize :net_mothly_income_cents, :numericality=>{:greater_than_or_equal_to=>0}
+    monetize :net_monthly_income_cents, :numericality=>{:greater_than_or_equal_to=>0}
     monetize :net_monthly_expense_cents, :numericality=>{:greater_than_or_equal_to=>0}
 
-    attr_accessible :net_mothly_income, :net_monthly_expense, :person_id
+    attr_accessible :net_monthly_income, :net_monthly_expense, :person_id
 
     belongs_to :person
   end
--- db/migrate/20130629144307_create_financial_finances.rb
+++ db/migrate/20130629144307_create_financial_finances.rb
@@ -1,7 +1,7 @@
 class CreateFinancialFinances < ActiveRecord::Migration
   def change
     create_table :financial_finances do |t|
-      t.money :net_mothly_income
+      t.money :net_monthly_income
       t.money :net_monthly_expense
       t.column :person_id, :integer
       t.timestamps
