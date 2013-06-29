96b948fd2c1b38141138ba0f986ee3d75d7796e4
Thai Le <lnthai2002@gmail.com>
2013-06-29 14:48:12 -0400

    issue_62: fix typo in column name of Finance

--- app/models/financial/finance.rb
+++ app/models/financial/finance.rb
@@ -1,7 +1,7 @@
 module Financial
   class Finance < ActiveRecord::Base
     monetize :net_mothly_income_cents, :numericality=>{:greater_than_or_equal_to=>0}
-    monetize :net_monthly_expense, :numericality=>{:greater_than_or_equal_to=>0}
+    monetize :net_monthly_expense_cents, :numericality=>{:greater_than_or_equal_to=>0}
 
     attr_accessible :net_mothly_income, :net_monthly_expense, :person_id
 
