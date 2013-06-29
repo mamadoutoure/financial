0f21771c430088c6925c546d22076356337ab4b3
Thai Le <lnthai2002@gmail.com>
2013-06-29 12:09:06 -0400

    issue_63: replace all reference of budget in plan controller

--- app/controllers/financial/plans_controller.rb
+++ app/controllers/financial/plans_controller.rb
@@ -1,34 +1,34 @@
 require_dependency "financial/application_controller"
 
 module Financial
-  class BudgetsController < ApplicationController
+  class PlansController < ApplicationController
     def index
-      @budget = Budget.new
+      @plan = Plan.new
       #see http://stackoverflow.com/questions/4867880/nested-attributes-in-rails-3
-      @budget.build_mortgage
-      @budget.build_investment
-      @budgets = Budget.all
+      @plan.build_mortgage
+      @plan.build_investment
+      @plans = Plan.all
     end
 
     def create
-      @budget = Budget.new(params[:budget])
-      if @budget.save #valid budget
-        @saved_budget = @budget #used to render recently added budget
-        @budget = Budget.new(params[:budget]) #used to populate form
+      @plan = Plan.new(params[:plan])
+      if @plan.save #valid plan
+        @saved_plan = @plan #used to render recently added plan
+        @plan = Plan.new(params[:plan]) #used to populate form
       else
-        #return the budget with error to be displayed
+        #return the plan with error to be displayed
       end
-      render "add_new_budget"
+      render "add_new_plan"
     end
 
     def destroy
-      @budget = Budget.find(params[:id])
-      @budget.destroy
-      render "delete_budget"
+      @plan = Plan.find(params[:id])
+      @plan.destroy
+      render "delete_plan"
     end
 
     def show
-      @budget = Budget.find(params[:id])
+      @plan = Plan.find(params[:id])
     end
   end
 end
